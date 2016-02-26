4e7b044b2e08655723063c7eea2a034b3e00b407
Lawrence Hsia <ray.hsia@gmail.com>
2016-02-26 02:54:34 -0600

    Added listviews with icons and subtext descriptions.

--- app/src/main/java/com/example/todd/baseui/DevicesFragment.java
+++ app/src/main/java/com/example/todd/baseui/DevicesFragment.java
@@ -5,12 +5,35 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class DevicesFragment extends Fragment {
+
+    ListView listView;
+    String[] deviceNames = {"Humpus Wumpus", "Cool Guy", "Radiated Guy", "Crome guy"};
+    String[] descriptions = {"bump the humpus", "2Kool", "Radiated, don't touch", "croam"};
+    int[] imageIds = {R.drawable.ie_icon, R.drawable.cool_icon, R.drawable.rad_icon, R.drawable.chrome_icon};
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.devices_tab, container, false);
+        View rootView = inflater.inflate(R.layout.devices_tab, container, false);
+        listView = (ListView) rootView.findViewById(R.id.list);
+
+        ImageListAdapter imgListAdapter = new ImageListAdapter(getActivity(),  deviceNames, imageIds, descriptions);
+        listView.setAdapter(imgListAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast toast = Toast.makeText(getActivity(), deviceNames[position], Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+
+        return rootView;
     }
 }
---
+++ app/src/main/java/com/example/todd/baseui/ImageListAdapter.java
@@ -0,0 +1,51 @@
+package com.example.todd.baseui;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by Ray on 2/26/2016.
+ */
+public class ImageListAdapter extends ArrayAdapter<String>{
+
+    private Context context;
+    String devices[];
+    int[] images;
+    String descriptions[];
+    LayoutInflater layoutInflater;
+
+    public ImageListAdapter(Context c, String[] dev, int[] imgs, String desc[])
+    {
+        super(c, R.layout.row_model, dev);
+
+        context = c;
+        devices = dev;
+        images = imgs;
+        descriptions = desc;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if(convertView == null)
+        {
+            layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(R.layout.row_model, null);
+        }
+
+        TextView device = (TextView) convertView.findViewById(R.id.header_txt);
+        ImageView image = (ImageView ) convertView.findViewById(R.id.device_icon);
+        TextView description = (TextView) convertView.findViewById(R.id.subheader_txt);
+
+        device.setText(devices[position]);
+        image.setImageResource(images[position]);
+        description.setText(descriptions[position]);
+
+        return convertView;
+    }
+}
