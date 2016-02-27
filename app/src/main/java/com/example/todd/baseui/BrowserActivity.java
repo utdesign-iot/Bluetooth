323468966740f5e5a67f38ed6ce1e988356e2ed2
Todd Thompson <todd.thompson.mail@gmail.com>
2016-02-27 17:10:57 -0600

    updated  Devices listview to start an activity with a url from listitem description

---
+++ app/src/main/java/com/example/todd/baseui/BrowserActivity.java
@@ -0,0 +1,25 @@
+package com.example.todd.baseui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+/**
+ * Created by Todd on 2/27/2016.
+ */
+public class BrowserActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about_activty);
+
+        String url = getIntent().getStringExtra(DevicesFragment.URL);
+
+        WebView webView = (WebView) findViewById(R.id.about_webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(url);
+    }
+}
--- app/src/main/java/com/example/todd/baseui/DevicesFragment.java
+++ app/src/main/java/com/example/todd/baseui/DevicesFragment.java
@@ -1,5 +1,6 @@
 package com.example.todd.baseui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -12,25 +13,42 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 public class DevicesFragment extends Fragment {
-
+    public final static String URL = "http://ecs.utdallas.edu";
     ListView listView;
-    String[] deviceNames = {"Humpus Wumpus", "Cool Guy", "Radiated Guy", "Crome guy"};
-    String[] descriptions = {"bump the humpus", "2Kool", "Radiated, don't touch", "croam"};
-    int[] imageIds = {R.drawable.ie_icon, R.drawable.cool_icon, R.drawable.rad_icon, R.drawable.chrome_icon};
+    String[] deviceNames = {
+            "Humpus Wumpus",
+            "Cool Guy",
+            "Radiated Guy",
+            "Crome guy"};
+    String[] descriptions = {
+            "http://utdallas.edu",
+            "http://google.com",
+            "http://yahoo.com",
+            "http://ecs.utdallas.edu"};
+    int[] imageIds = {R.drawable.ie_icon,
+            R.drawable.cool_icon,
+            R.drawable.rad_icon,
+            R.drawable.chrome_icon};
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.devices_tab, container, false);
+        View rootView = inflater.inflate(R.layout.devices_tab,
+                container, false);
         listView = (ListView) rootView.findViewById(R.id.list);
 
-        ImageListAdapter imgListAdapter = new ImageListAdapter(getActivity(),  deviceNames, imageIds, descriptions);
+        ImageListAdapter imgListAdapter =
+                new ImageListAdapter(getActivity(), deviceNames, imageIds, descriptions);
         listView.setAdapter(imgListAdapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast toast = Toast.makeText(getActivity(), deviceNames[position], Toast.LENGTH_SHORT);
-                toast.show();
+                //Toast toast = Toast.makeText(getActivity(), deviceNames[position], Toast.LENGTH_SHORT);
+                //toast.show();
+                Intent intent = new Intent(getContext(), BrowserActivity.class);
+                intent.putExtra(URL, descriptions[position]);
+                startActivity(intent);
             }
         });
 
